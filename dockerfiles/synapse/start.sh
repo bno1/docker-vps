9f43fb8bb1583341988b5b312b25d2aaa5b1ea2b
bno1 <ealex95@gmail.com>
2019-01-30 14:10:56 +0000

    update

--- dockerfiles/synapse/start.sh
+++ dockerfiles/synapse/start.sh
@@ -4,4 +4,4 @@ set -ex
 
 envsubst '${SYNAPSE_DB_PASSWORD} ${SYNAPSE_POSTGRES_HOST} ${SYNAPSE_PEPPER}' < /data/homeserver.yaml.template > /data/homeserver.yaml
 
-exec python -m synapse.app.homeserver -c /data/homeserver.yaml
+exec python3 -m synapse.app.homeserver -c /data/homeserver.yaml
