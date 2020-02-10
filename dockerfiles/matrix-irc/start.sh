20bcf0ca3865f7ad6038442e64bb6a9570879ccb
bno1 <ealex95@gmail.com>
2020-02-10 23:02:27 +0000

    update

--- dockerfiles/matrix-irc/start.sh
+++ dockerfiles/matrix-irc/start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+BIN="$HOME/matrix-appservice-irc/bin/matrix-appservice-irc"
+
 if [ ! -f /app-services/irc.yaml ]; then
-    matrix-appservice-irc -r -f /app-services/irc.yaml -u "http://matrix-irc:9999" -c /data/config.yaml -l "$MATRIX_IRC_BOTNAME"
+    "$BIN" -r -f /app-services/irc.yaml -u "http://matrix-irc:9999" -c /data/config.yaml -l "$MATRIX_IRC_BOTNAME"
 fi
 
-exec matrix-appservice-irc -c /data/config.yaml -f /app-services/irc.yaml -p 9999
+exec "$BIN" -c /data/config.yaml -f /app-services/irc.yaml -p 9999
